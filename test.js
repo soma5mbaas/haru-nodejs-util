9d2ab9c13bf7a52129f741fba6849d67a374dca2
syntaxfish <syntaxfish@gmail.com>
2014-10-10 22:19:09 +0900

    haru-nodejs-util 버전업 0.0.6 -> 0.0.7

--- test.js
+++ test.js
@@ -1,29 +1,29 @@
 
 var utils = require('./index');
 
-var keys = utils.keys;
-console.log( keys.schemaKey('asd','123') );
+// var keys = utils.keys;
+// console.log( keys.schemaKey('asd','123') );
 
 var common = utils.common;
-console.log( common.exportSchema({ name: "asd", age: 12}) );
+// console.log( common.exportSchema({ name: "asd", age: 12}) );
 
-var errorCode = utils.errorCode;
-console.log(errorCode.INVALID_USER_TOKEN);
+// var errorCode = utils.errorCode;
+// console.log(errorCode.INVALID_USER_TOKEN);
 
-var Logger = utils.Logger;
+// var Logger = utils.Logger;
 
-var log = new Logger('./error.txt');
-log.info('asd');
+// var log = new Logger('./error.txt');
+// log.info('asd');
 
-var parseToJson = common.parseToJson;
-var sjon = { age: '11'};
-var schema = { age: 'number'};
-console.log(  typeof parseToJson(schema, sjon).age );
+// var parseToJson = common.parseToJson;
+// var sjon = { age: '11'};
+// var schema = { age: 'number'};
+// console.log(  typeof parseToJson(schema, sjon).age );
 
-var parseToJsons = common.parseToJsons;
-var sjons = [ { age: '11'}, {name: 'park', age:'25'} ] ;
-var schema2 = { age: 'number', name: 'string'};
-console.log(  typeof parseToJsons(schema2, sjons)[0].age );
+// var parseToJsons = common.parseToJsons;
+// var sjons = [ { age: '11'}, {name: 'park', age:'25'} ] ;
+// var schema2 = { age: 'number', name: 'string'};
+// console.log(  typeof parseToJsons(schema2, sjons)[0].age );
 
 var isEmptyObject = common.isEmptyObject;
 var obj1 = {};
